31b9ce71679ba549f5d0509012b147c9e3e7189c
HovhannesTutyan <atutyanaz@gmail.com>
2024-07-24 06:46:12 +0400

    small fixes

--- GetData/GetDataMarketCupSol.py
+++ GetData/GetDataMarketCupSol.py
@@ -17,42 +17,50 @@ django.setup()
 from main.models import Coin, HistoryData
 
 # Constants
-COIN_ID = 'solana'
-API_URL = f'https://api.coingecko.com/api/v3/coins/{COIN_ID}/market_chart'
+COINS = [
+    {'id': 'solana', 'name': 'Solana', 'symbol': 'SOL'},
+    {'id': 'bitcoin', 'name': 'Bitcoin', 'symbol': 'BTC'}
+]
 DAYS = 365
-# Fetch historical data from CoinGecko
-response = requests.get(API_URL, params={'vs_currency': 'usd', 'days': DAYS})
-data = response.json()
-
-# Ensure Coin exists in database
-solana, created = Coin.objects.get_or_create(name='Solana', symbol='SOL')
-
+all_data = []
 by_symbol = []
-# # Process and store historical data
-for i in range(len(data['market_caps'])):
-    timestamp, market_cap = data['market_caps'][i]
-    price_usd = data['prices'][i][1]
-    volume_24h = data['total_volumes'][i][1]
-    date = datetime.utcfromtimestamp(timestamp / 1000).date()
-    data_point = {
-        "date": date.isoformat(),
-        "price_usd": price_usd,
-        "market_cap": market_cap,
-        "volume_24h": volume_24h
-    }
-
-    # Add the data point to the list
-    by_symbol.append(data_point)
-    # Avoid duplications
-    if not HistoryData.objects.filter(coin=solana, date=date).exists():
-        HistoryData.objects.create(
-            coin=solana,
-            date=date.isoformat(),
-            market_cap=market_cap,
-            price_usd=price_usd,
-            volume_24h=volume_24h,
-        )
-
+for coin_info in COINS:
+    COIN_ID = coin_info["id"]
+    API_URL = f'https://api.coingecko.com/api/v3/coins/{COIN_ID}/market_chart'
+    # Fetch historical data from CoinGecko
+    response = requests.get(API_URL, params={'vs_currency': 'usd', 'days': DAYS})
+    data = response.json()
+    # Ensure Coin exists in database
+    coin, created = Coin.objects.get_or_create(name=coin_info["name"], symbol=coin_info["symbol"])
+    # Process and store historical data
+    for i in range(len(data['market_caps'])):
+        timestamp, market_cap = data['market_caps'][i]
+        price_usd = data['prices'][i][1]
+        volume_24h = data['total_volumes'][i][1]
+        date = datetime.utcfromtimestamp(timestamp / 1000).date()
+        data_point = {
+            "name":coin_info["name"],
+            "symbol":coin_info["symbol"],
+            "date": date.isoformat(),
+            "price_usd": price_usd,
+            "market_cap": market_cap,
+            "volume_24h": volume_24h
+        }
+        # Add the data point to the list
+        by_symbol.append(data_point)
+        # Avoid duplications
+        if not HistoryData.objects.filter(coin=coin, date=date).exists():
+            HistoryData.objects.create(
+                coin=coin,
+                date=date.isoformat(),
+                market_cap=market_cap,
+                price_usd=price_usd,
+                volume_24h=volume_24h,
+            )
+    all_data.append({
+        "coin": coin_info["name"],
+        "data": by_symbol
+    })
 with open("solana.json", 'w') as json_file:
     json.dump(by_symbol, json_file, indent=4)
 
